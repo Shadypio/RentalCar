08cf905e696e37664495b55d896b32269039468a
Si2001 <Si2001@Si-W-00576.station>
2022-12-28 16:57:40 +0100

    Added View for all Utente

--- rentalcar/src/main/java/com/car/webapp/controller/UtenteController.java
+++ rentalcar/src/main/java/com/car/webapp/controller/UtenteController.java
@@ -1,5 +1,38 @@
 package com.car.webapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.car.webapp.domain.utente.Utente;
+import com.car.webapp.service.utente.IUtenteService;
+
+@Controller
+@RequestMapping("/utente")
 public class UtenteController {
+	
+	@Autowired
+	private IUtenteService utenteService;
+	
+	private List<Utente> recordset;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getUtenti(Model model) {
+		
+		recordset = utenteService.getAllUtenti();
+		
+		model.addAttribute("Titolo", "Ricerca Utenti");
+		model.addAttribute("Titolo2", "Ricerca tutti gli utenti");
+		model.addAttribute("Utenti", recordset);
+		model.addAttribute("isUtente", true);
+		if (recordset != null)
+			model.addAttribute("numUtenti", recordset.size());
+		
+		return "utenti";
+	}
 
 }
