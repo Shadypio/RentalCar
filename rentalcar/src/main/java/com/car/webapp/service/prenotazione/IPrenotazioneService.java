e74a4329f57909105a20c4bd10cd5f72d50426ee
Shadypio <enzuccioamendola@gmail.com>
2023-01-10 17:11:20 +0100

    Added PrenotazioneService class for service

---
+++ rentalcar/src/main/java/com/car/webapp/service/prenotazione/IPrenotazioneService.java
@@ -0,0 +1,20 @@
+package com.car.webapp.service.prenotazione;
+
+import java.util.List;
+
+import com.car.webapp.domain.prenotazione.Prenotazione;
+
+public interface IPrenotazioneService {
+
+	List <Prenotazione> getAllPrenotazioni();
+	
+	Prenotazione selPrenotazioneById(Long id);
+	
+	void insPrenotazione(Prenotazione prenotazione);
+	
+	void modificaPrenotazione(Prenotazione prenotazione);
+	
+	void delPrenotazione(Prenotazione prenotazione);
+	
+	void delUtenteById(Long id);
+}
