76dc241292b97020a71679993b8c5367bea8e9c0
Shadypio <enzuccioamendola@gmail.com>
2023-01-12 18:03:34 +0100

    JSP for login

---
+++ rentalcar/src/main/java/com/car/webapp/config/LoginController.java
@@ -0,0 +1,18 @@
+package com.car.webapp.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login/form")
+public class LoginController 
+{
+	@GetMapping
+	public String getLogin(Model model)
+	{
+		return "login";
+	}
+}
+
