5da6bbea65c4422afef1eb8712a197de8c149d1c
Shadypio <enzuccioamendola@gmail.com>
2023-01-12 09:36:52 +0100

    Cleaning code

--- rentalcar/src/main/java/com/car/webapp/controller/IndexController.java
+++ rentalcar/src/main/java/com/car/webapp/controller/IndexController.java
@@ -7,20 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController 
 {
-	@RequestMapping(value="index")
-	public String getWelcome(Model model)
-	{
-		model.addAttribute("intestazione", "Benvenuti nel sito RentalCar");
-		model.addAttribute("saluti", "Seleziona le auto da noleggiare");
-		
-		return "index";
-	}
 	
 	@RequestMapping(value="/")
 	public String getWelcome2(Model model)
 	{
-		model.addAttribute("intestazione", "Benvenuti nel sito RentalCar 2");
-		model.addAttribute("saluti", "Seleziona le auto da noleggiare 2");
+		model.addAttribute("intestazione", "Benvenuti nel sito RentalCar");
+		model.addAttribute("saluti", "Seleziona le auto da noleggiare");
 		
 		return "index";
 	}
