030af1139b8790b9574a58f12a57cf034743d2e3
Shadypio <enzuccioamendola@gmail.com>
2023-01-13 10:16:23 +0100

    Form insert new Utente

--- rentalcar/src/main/java/com/car/webapp/controller/UtenteController.java
+++ rentalcar/src/main/java/com/car/webapp/controller/UtenteController.java
@@ -1,17 +1,24 @@
 package com.car.webapp.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.car.webapp.domain.utente.Utente;
 import com.car.webapp.service.utente.IUtenteService;
@@ -24,103 +31,30 @@ public class UtenteController {
 
 	@Autowired
 	private IUtenteService utenteService;
+	
+	// Codifica password
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
-	List<Utente> mainRecordset;
-
-	private String orderType = "DESC";
-	private int orderBy = 0;
-
-	//List<PagingData> Pages = new ArrayList<PagingData>();
-	private int pageNum = 1;
-	private int recForPage = 10;
-
-	private boolean isClienti = true;
+	List<Utente> recordset;
 
 	// Equivalente a @GetMapping
 	@RequestMapping(method = RequestMethod.GET)
 	public String getUtenti(Model model) {
 
-		List<Utente> recordset = null;
-		long numRecords = 0;
+		
 
 		logger.info("Otteniamo tutti gli utenti");
-		mainRecordset = utenteService.getAllUtenti();
-
-		/*
-		 * 
-		 
-		if(mainRecordset != null) {
-
-			numRecords = mainRecordset.size();
-		}
-
-		recordset = mainRecordset
-				.stream()
-				.skip(0)
-				.limit(recForPage)
-				.collect(Collectors.toList());
-		
-		
-		
-		if (recordset != null)
-			model.addAttribute("numUtenti", recordset.size());
-
-		logger.info("Numero di record per pagina: " + recForPage);
-		setPages(pageNum);
-		*/
+		recordset = utenteService.getAllUtenti();
 
 		model.addAttribute("Titolo", "Ricerca Utenti");
 		model.addAttribute("Titolo2", "Ricerca tutti gli utenti");
-		model.addAttribute("Utenti", mainRecordset);
-		model.addAttribute("numUtenti", mainRecordset.size());
+		model.addAttribute("Utenti", recordset);
+		model.addAttribute("numUtenti", recordset.size());
 		model.addAttribute("isUtente", true);
 		return "utenti";
 	}
 
-	/**
-	 * 
-	//Metodo di creazione classi Pages
-	private void setPages(int Page, long numRecords)
-	{
-		int Min = 1;
-		int ValMin = 1;
-		int Max = 5;
-
-		if (Pages != null)
-			Pages.clear();
-
-		int Group = (int) Math.ceil((double)Page / 5);
-
-		Max = Group * 5;
-		Min = (Max - 5 == 0) ? 1 : Max - 4;
-
-		ValMin = Min;
-
-		int MaxPages = (numRecords > 0) ? (int) Math.ceil((double)numRecords / (double)RecForPage) : 5;
-
-		while (Min <= Max)
-		{
-			if (Min > MaxPages)
-				break;
-
-			Pages.add(new PagingData(Min,false));
-
-			Min++;
-		}
-
-		try
-		{
-			if (Page - ValMin > 0)
-				Pages.get(Page - ValMin).setIsSelected(true);
-			else
-				Pages.get(0).setIsSelected(true);
-		}
-		catch (Exception ex)
-		{
-			Pages.get(0).setIsSelected(true);
-		}
-	}
-	*/
 	
 	@GetMapping(value = "/elimina/{idUtente}")
 	public String delUtente(@PathVariable("idUtente") Long idUtente, Model model)
@@ -165,6 +99,39 @@ public class UtenteController {
 		}
 
 		return "infoUtente";
+	}
+	
+	@GetMapping(value = "/aggiungi")
+	public String insUtente(Model model) {
+		
+		Utente utente = new Utente();
+		
+		model.addAttribute("Titolo", "Inserimento Nuovo Utente");
+		model.addAttribute("utente", utente);
+		model.addAttribute("edit", false);
+		model.addAttribute("saved", false);
+		
+		
+		return "insUtente";
+	}
+	
+	@PostMapping(value = "/aggiungi")
+	public String gestInsUtente(@Valid @ModelAttribute("utente") Utente nuovoUtente, BindingResult result, Model model, 
+			RedirectAttributes redirectAttributes, HttpServletRequest request) {
+		
+		if(result.hasErrors()) {
+			return "insUtente";
+		}
+		
+		System.out.println(nuovoUtente.toString());
+		
+		nuovoUtente.setPassword(passwordEncoder.encode(nuovoUtente.getPassword()));
+		
+		utenteService.insUtente(nuovoUtente);
+		
+		redirectAttributes.addFlashAttribute("saved", true);
+		
+		return "redirect:/utente/infoutente/" + nuovoUtente.getUsername();
 	}
 
 }
--- rentalcar/src/main/webapp/WEB-INF/view/insUtente.jsp
+++ rentalcar/src/main/webapp/WEB-INF/view/insUtente.jsp
@@ -43,49 +43,53 @@
 					</div>
 				</c:if>
 				
-				<!-- Form Dati Auto -->
+				<!-- Form Dati Utente -->
 				<div class="tab-pane fade show active" id="dati" role="tabpanel" aria-labelledby="dati-tab">
-					<form:form  method="POST" modelAttribute="auto">
+					<form:form  method="POST" modelAttribute="utente">
 					<form:errors path="*" cssClass="alert alert-danger" element="div"/> 
 					
 					<div class="form-body">
 					
 						<div class="form-group">
-							<label for="targa"><spring:message code="insauto.form.targa.label"/></label>
-							<form:input id="targa" path="targa" type="text" class="form-control" placeholder="Targa"/>  
-							<form:errors path="targa" cssClass="text-danger"/>
+							<label for="nome"><spring:message code="insauto.form.targa.label"/></label>
+							<form:input id="nome" path="nome" type="text" class="form-control" placeholder="Nome"/>  
+							<form:errors path="nome" cssClass="text-danger"/>
 						</div>
 						
 						<div class="form-group">
-							<label for="marca"><spring:message code="insauto.form.marca.label"/></label>
-							<form:input id="marca" path="marca" type="text" class="form-control" placeholder="Marca"/>  
-							<form:errors path="marca" cssClass="text-danger"/>
+							<label for="cognome"><spring:message code="insauto.form.marca.label"/></label>
+							<form:input id="cognome" path="cognome" type="text" class="form-control" placeholder="Cognome"/>  
+							<form:errors path="cognome" cssClass="text-danger"/>
 						</div>
 						
 						<div class="form-group">
-							<label for="modello"><spring:message code="insauto.form.modello.label"/></label>
-							<form:input id="modello" path="modello" type="text" class="form-control" placeholder="Modello"/>  
-							<form:errors path="modello" cssClass="text-danger"/>
+							<label for="dataDiNascita"><spring:message code="insauto.form.categoria.label"/></label>
+							<form:input id="dataDiNascita" path="dataDiNascita" type="date" 
+							min="1950-01-01" max="2004-12-31"
+							class="form-control" placeholder="Data di nascita"/>  
+							<form:errors path="dataDiNascita" cssClass="text-danger"/>
 						</div>
 						
 						<div class="form-group">
-							<label for="categoria"><spring:message code="insauto.form.categoria.label"/></label>
-							<form:input id="categoria" path="categoria" type="text" class="form-control" placeholder="Categoria"/>  
-							<form:errors path="categoria" cssClass="text-danger"/>
+							<label for="username"><spring:message code="insauto.form.modello.label"/></label>
+							<form:input id="username" path="username" type="text" class="form-control" placeholder="Username"/>  
+							<form:errors path="username" cssClass="text-danger"/>
 						</div>
 						
 						<div class="form-group">
-							<label for="anno"><spring:message code="insauto.form.anno.label"/></label>
-							<form:input id="anno" path="anno" type="text" class="form-control" placeholder="Anno di immatricolazione"/>  
-							<form:errors path="anno" cssClass="text-danger"/>
+							<label for="password"><spring:message code="insauto.form.categoria.label"/></label>
+							<form:input id="password" path="password" type="text" class="form-control" placeholder="Password"/>  
+							<form:errors path="password" cssClass="text-danger"/>
 						</div>
+						
+						
 						
 						<hr class="line-form">
 						
 						<div class="form-actions">
 							<input type="submit" id="btnAdd" class="btn btn-green form-buttons" value = <spring:message code="insauto.form.btnAdd.label"/> />
 							
-							<a href="<spring:url value="/auto/" /> " id="btnAbort" class="btn btn-default form-buttons" >
+							<a href="<spring:url value="/clienti/" /> " id="btnAbort" class="btn btn-default form-buttons" >
 								<spring:message code="insauto.form.btnAbort.label"/>
 							</a>
 						</div>
