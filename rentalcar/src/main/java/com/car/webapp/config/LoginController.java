75aa51c9a21b66b3c7078ff184f86a822cd2894c
Shadypio <enzuccioamendola@gmail.com>
2023-01-13 16:42:43 +0100

    Organized Imports

--- rentalcar/src/main/java/com/car/webapp/config/LoginController.java
+++ rentalcar/src/main/java/com/car/webapp/config/LoginController.java
@@ -9,15 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.car.webapp.service.utente.IUtenteService;
 
 
 @Controller
